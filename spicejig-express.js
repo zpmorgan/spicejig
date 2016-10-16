82e466d1672783c0c30aec806fe26aec5db5e62a
Zach Morgan <zpmorgan@gmail.com>
2016-10-16 14:48:36 -0400

    phaser state & reddit scraping logic

--- spicejig-express.js
+++ spicejig-express.js
@@ -1,7 +1,8 @@
 //var http = require("http");
 var express = require('express');
+var request = require('request');
+//var cheerio = require('cheerio');
 var app = express();
-
 app.use(express.static('static'));
 
 app.get('/',function(req, res) {
@@ -11,7 +12,67 @@ app.get('/',function(req, res) {
   res.json({query: ttt});
 });
 
+function rand_reddit_thing(){ //return a promise
+  var url = "https://www.reddit.com/r/ImaginaryMindscapes/.json";
+  var p = new Promise(function(resolve,reject){
+    request(url, function(err,scrape_res, res_json){
+      if(err) { res.json("blehblah "+ err.code); return;}
+      if (scrape_res.statusCode !== 200) { res.json("code "+ scrape_res.statusCode); return;}
+      var res_parsed = JSON.parse(res_json);
+      if(res_parsed.kind !== "Listing") { res.json(res_parsed.kind); return;}
+      var things = res_parsed.data.children;
+      jpg_regex = /\.jpe?g(\?.*)?$/;
+      nothings = things.filter(function(t){return ! t.data.url.match(jpg_regex)});
+      things = things.filter(function(t){return t.data.url.match(jpg_regex)});
+      var thing = things[Math.floor(Math.random()*things.length)];
+      resolve(thing);
+    });
+  });
+  return p;
+}
+app.get('/scrapejson', function(req,res){
+  rand_reddit_thing().then(function(thing){
+    res.json(thing);
+  });
+});
+
+app.get('/t3/:t3id', (req,res) => {
+  res.sendFile('/home/zach/codestuff/scratch/spicejig/' + 'static/images/scream.jpg');
+});
+
+app.get('/scrape', function(req,res){
+  var url = "https://www.reddit.com/r/ImaginaryBestOf";
+  request(url, function(err,scrape_res,html){
+    if(err) return;
+    var $ = cheerio.load(html);
+    var entries = $('.thing');
+    var entry = entries.get(Math.floor(Math.random()*entries.length));
+    console.log(Object.getOwnPropertyNames(entries));
+    console.log(entry);
+    console.log(entry.attr());
+    return;
+    var entries = $('.thing').toArray();
+    console.log(entries[0]);
+    res.json(entries[0].attr('data-url'));
+    var entry = entries[Math.floor(Math.random()*entries.length)];
+    return;
+    console.log(entries);
+    console.log(entries.toArray().length);
+                return;
+    //console.log(entries);
+    var entry = entries[Math.floor(Math.random()*entries.length)];
+    console.log(entries);
+    console.log(entry.data('url'));
+    console.log(entries[0].data('url'));
+    res.json(entries[0].attr('data-url'));
+  });
+});
+app.get('/new_puz_spec', function(req,res){
+  var thing = rand_reddit_thing().then( (tng) => {
+    res.json(tng);
+  });
+  //res.json({img_url : thing.img_url});
+});
+
 app.listen(8888);
-
-
 
--- static/js/game.js
+++ static/js/game.js
@@ -5,17 +5,55 @@ requirejs(['domReady', 'phaser', 'puzzle'], function(domReady){
   domReady(function() {
     game = new Phaser.Game(800, 500);
     game.state.add("PlayGame", playGame)
-    game.state.start("PlayGame");
+    game.state.add("Boot", boot)
+    game.state.start("Boot");
   });
 });
+
+function loadJSON(path, success, error)
+{
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function()
+  {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+      if (xhr.status === 200) {
+        if (success)
+          success(JSON.parse(xhr.responseText));
+      } else {
+        //if (error)
+        console.error(xhr);
+      }
+    }
+  };
+  xhr.open("GET", path, true);
+  xhr.send();
+}
+
+var boot = function(game){}
+boot.prototype = {
+  preload: function(){
+    game.load.json('spec', '/new_puz_spec');
+  },
+  create: () => {
+    game.state.start('PlayGame');
+  }
+};
 
 var playGame = function(game){}
 playGame.prototype = {
 	preload: function(){
-    game.load.image("scream", "images/scream.jpg");
+    //game.load.image("scream", "images/scream.jpg");
+    var spec = game.cache.getJSON('spec');
+    game.load.image('scream', '/t3/' + spec.data.id);
+    console.log('loading image ' + spec.data.id);
 	},
 	create: function(){
     game.canvas.oncontextmenu = function (e) { e.preventDefault(); }
+    //game.load.image("scream", "images/scream.jpg");
+    //game.load.image('scream', '/t3/' + spec.data.id);
+    //game.load.image("scream", "images/scream.jpg");
+    //game.load.start();
+    //game.load.image('scream', spec.data.url);
     puzzle = new Puzzle(game, "scream", 3, 3);
   }
 }
